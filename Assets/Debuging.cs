d2b47e7a400fd86a432bbddd10ee37cca37f2d02
antek1085 <antek1085@o2.pl>
2024-04-07 07:40:18 +0200

    Fix

--- Assets/Debuging.cs
+++ Assets/Debuging.cs
@@ -1,7 +1,6 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Reflection;
 using UnityEngine;
 
 namespace UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets
@@ -22,25 +21,6 @@ namespace UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets
         {
             Debug.Log("dupa off");
             provider.useGravity = false;
-        }
-
-        public void CheckWhatITouch()
-        {
-            Debug.Log("Objects currently grabbed for climbing:");
-
-            // Użyj refleksji do uzyskania dostępu do prywatnego pola m_GrabbedClimbables
-            FieldInfo grabbedClimbablesField = typeof(ClimbProvider).GetField("m_GrabbedClimbables", BindingFlags.NonPublic | BindingFlags.Instance);
-            if (grabbedClimbablesField != null)
-            {
-                List<ClimbInteractable> grabbedClimbables = (List<ClimbInteractable>)grabbedClimbablesField.GetValue(climbProvider);
-                if (grabbedClimbables != null)
-                {
-                    foreach (var climbInteractable in grabbedClimbables)
-                    {
-                        Debug.Log(climbInteractable.gameObject.name);
-                    }
-                }
-            }
         }
     }
 }
