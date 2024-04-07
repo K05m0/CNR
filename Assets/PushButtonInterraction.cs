736ab5279c7ed5c5c554008775eb95415415144b
ted <wojcikkuba414@gmail.com>
2024-04-07 18:21:36 +0200

    Start

--- Assets/PushButtonInterraction.cs
+++ Assets/PushButtonInterraction.cs
@@ -1,32 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Samples.StarterAssets;
 
 public class PushButtonInterraction : MonoBehaviour
 {
-    [SerializeField] List<GameObject> Ui = new List<GameObject>();
-    [SerializeField] int index = 0;
-    private int objectToChange;
-   public void OnPush()
+    [SerializeField] private DynamicMoveProvider dynamicMoveProvider;
+
+    private void Awake()
     {
-        objectToChange = index;
-        switch (objectToChange)
-        {
-            case 0:
-                Ui[0].SetActive(true);
-                Ui[1].SetActive(false);
-                StartCoroutine(LeaveGame());
-                break;
-            case 1:
-                Ui[0].SetActive(false);
-                Ui[1].SetActive(true);
-                break;
-        }
+        dynamicMoveProvider.moveSpeed = 0;
     }
 
-    IEnumerator LeaveGame()
+    public void OnStart()
     {
-        yield return new WaitForSeconds(4);
-        Application.Quit();
+        dynamicMoveProvider.moveSpeed = 3;
     }
+    /*    [SerializeField] List<GameObject> Ui = new List<GameObject>();
+        [SerializeField] int index = 0;
+        private int objectToChange;
+       public void OnPush()
+        {
+            objectToChange = index;
+            switch (objectToChange)
+            {
+                case 0:
+                    Ui[0].SetActive(true);
+                    Ui[1].SetActive(false);
+                    StartCoroutine(LeaveGame());
+                    break;
+                case 1:
+                    Ui[0].SetActive(false);
+                    Ui[1].SetActive(true);
+                    break;
+            }
+        }
+
+        IEnumerator LeaveGame()
+        {
+            yield return new WaitForSeconds(4);
+            Application.Quit();
+        }*/
 }
